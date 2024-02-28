50ccde7a457bc7a9c43d157cdf572b7eb710165c
Victor Mosomi Ochako <145469157+vochak@users.noreply.github.com>
2024-02-28 10:16:27 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What's your name? ")
+age = input("How old are you? ")
+location = input("Where are you located? ")
+message = f"Hello, {name}! It's great to know that you're {age} years old and located in {location}."
+print(message)
